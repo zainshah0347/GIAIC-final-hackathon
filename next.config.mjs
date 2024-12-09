e7b52274df9c87664ce177e2d3c10dba625677dd
zainshah0347 <sz03472363@gmail.com>
2024-12-09 23:02:08 +0500

    edit this file

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,11 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
